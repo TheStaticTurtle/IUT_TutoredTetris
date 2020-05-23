2e54f68aa3d32dbf1682374039b800eedbeaec9d
Samuel <tuglersamuel@gmail.com>
2020-05-23 22:38:38 +0200

    Added font + res folder

--- tetris/src/fr/iut/tetris/vues/MenuVue.java
+++ tetris/src/fr/iut/tetris/vues/MenuVue.java
@@ -1,11 +1,17 @@
 package fr.iut.tetris.vues;
 
+import fr.iut.tetris.Main;
 import fr.iut.tetris.controllers.MenuController;
 import fr.iut.tetris.models.MenuModel;
 
 import javax.swing.*;
 import javax.swing.text.Document;
 import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
 
 public class MenuVue extends JPanel {
 	MenuModel model;
@@ -15,6 +21,19 @@ public class MenuVue extends JPanel {
 		this.model = model;
 		this.ctrl = ctrl;
 		setPreferredSize(new Dimension( 640, 870 ));
-		add(new JLabel("ABCD"));
+
+		JLabel myLabel = new JLabel("ABCD");
+
+
+		try {
+			InputStream is = Main.class.getResourceAsStream("/res/retro.ttf");
+			Font font = Font.createFont(Font.TRUETYPE_FONT, is);
+			Font sizedFont = font.deriveFont(24f);
+			myLabel.setFont(sizedFont);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		add(myLabel);
 	}
 }
